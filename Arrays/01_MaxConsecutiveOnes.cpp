57490ec210c6102b7639b85a0ff44a17aa72ed65
Avez <aveznqureshi14@gmail.com>
2022-09-20 17:54:20 +0530

    d

--- Arrays/01_MaxConsecutiveOnes.cpp
+++ Arrays/01_MaxConsecutiveOnes.cpp
@@ -26,9 +26,10 @@ int findmaxiConsecutiveOnes(int nums[], int n)
 
 int main()
 {
-    int arr[] = {1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,1,1,1,1,1,0,0,0,};
+    int arr[] = {1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,1,1,1,1,1,0,0,0,};  
     int n = sizeof(arr) / sizeof(arr[0]);
     int res = findmaxiConsecutiveOnes(arr,n);
     cout<<"The maximum consecutive ones is "<<res<<endl;
     return 0;
 }
+ 
--- Practice.cpp
+++ Practice.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-vector<int> TwoSum(vector<int> nums, int target)
-{
-    vector<int> ans;
-    unordered_map<int, int> map;
+int subarraySum(vector<int>nums){
+    int currSum = 0;
+    int totalSum = INT_MIN;
     for (int i = 0; i < nums.size(); i++)
     {
-        if (map.find(target - nums[i]) != map.end())
+        currSum = currSum + nums[i];
+        if (currSum > totalSum)
         {
-            ans.push_back(map[target - nums[i]]);
-            ans.push_back(i);
+            totalSum = currSum;
         }
-        map[nums[i]] = i;
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+        totalSum = max(totalSum,currSum);        
     }
 
-    return ans;
+    return totalSum;
+    
 }
-
 int main()
 {
-    vector<int> nums;
-    nums.push_back(5);
-    nums.push_back(7);
-    nums.push_back(12);
-    nums.push_back(9);
-    nums.push_back(15);
-    nums.push_back(13);
-    nums.push_back(5);
+    vector<int>nums;
+    nums.push_back(-2);
+    nums.push_back(1);
+    nums.push_back(-3);
+    nums.push_back(4);
+    nums.push_back(-1);
     nums.push_back(2);
-
-    vector<int>res = TwoSum(nums,7);
-    for(auto i : res){
-        cout<<i<<" ";
-    }
+    nums.push_back(1);
+    nums.push_back(-5);
+    nums.push_back(4);
+    cout<<"Sum is "<<subarraySum(nums);
     return 0;
 }
