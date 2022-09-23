29179f6fcd34d8b28bf550585cfd2e3baedcc4f4
Avez <aveznqureshi14@gmail.com>
2022-09-23 16:40:12 +0530

    c

--- Experiment.cpp
+++ Experiment.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 int main()
 {
-   
-    int a = 1; int b = 2;
-    cout<<a%b
-;
 
+    int arr[] = {2,5,4};
+    int n = 3;
+   int sum = 0;
+   for (int i = 0; i < n; i++)
+   {
+    sum += arr[i];
+   }
+
+   cout<<sum;
+   
     return 0;
 }
